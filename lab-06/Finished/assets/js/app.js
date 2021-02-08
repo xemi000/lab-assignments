ebb3274b7925123b53ed9a9b2c7be0018db90ef1
xemi000 <se.zemichael.amsalu@gmail.com>
2021-02-08 22:46:34 +0300

    commited

--- lab-06/Finished/assets/js/app.js
+++ lab-06/Finished/assets/js/app.js
@@ -1,25 +1,4 @@
-// Define UI Variables 
-const taskInput = document.querySelector('#task'); //the task input text field
-const form = document.querySelector('#task-form'); //The form at the top
-const filter = document.querySelector('#filter'); //the task filter text field
-const taskList = document.querySelector('.collection'); //The UL
-const clearBtn = document.querySelector('.clear-tasks'); //the all task clear button
 
-const reloadIcon = document.querySelector('.fa'); //the reload button at the top navigation 
-
-// Add Event Listener  [Form , clearBtn and filter search input ]
-
-// form submit 
-form.addEventListener('submit', addNewTask);
-// Clear All Tasks
-clearBtn.addEventListener('click', clearAllTasks);
-//   Filter Task 
-filter.addEventListener('keyup', filterTasks);
-// Remove task event [event delegation]
-taskList.addEventListener('click', removeTask);
-// Event Listener for reload 
-reloadIcon.addEventListener('click', reloadPage);
-// DOM load event 
 document.addEventListener('DOMContentLoaded', loadTasksfromDB);
 
 
