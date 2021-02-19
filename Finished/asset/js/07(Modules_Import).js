6e79949f8293ab4327514539c025129c8bfa4f91
xemi000 <se.zemichael.amsalu@gmail.com>
2021-02-20 00:54:11 +0300

    commited

--- Finished/asset/js/07(Modules_Import).js
+++ Finished/asset/js/07(Modules_Import).js
@@ -1,33 +1,34 @@
 //1. Import the myVar as destructuring
+import { myVar } from "./07(Modules_Export).js";
 
 //2. Display the Var [remove the string when you have the variable]
-simple_import.innerHTML = "myVar";
-
+simple_import.innerHTML = myVar;
 
 //3. Import the exp1 fun as destructuring
+import { exp1 } from "./07(Modules_Export).js";
 
 //4. Display the exp1 [remove the string when you have the variable]
-list_import.innerHTML = "exp1()";
+list_import.innerHTML = exp1();
 
 //5. Import the aliased function as destructuring
-
+import { exp2alias } from "./07(Modules_Export).js";
 //6. Display the exp2_Alias [remove the string when you have the variable]
-exp_alias.innerHTML = "exp2_Alias()";
+exp_alias.innerHTML = exp2alias();
 
 //7. Import exp3 as alias function as destructuring
-
+import { exp3 as exp3_alias } from "./07(Modules_Export).js";
 //8. Display the exp3_alias() [remove the string when you have the variable]
-imp_alias.innerHTML = "exp3_alias()";
+imp_alias.innerHTML = exp3_alias();
 
-// 9 import All (*) as imp_all 
-
+// 9 import All (*) as imp_all
+import * as imp_all from "./07(Modules_Export).js";
 
 //10. Display on the UL
 import_all.innerHTML = `
 
-<li class="list-group-item" > imp_all.myVar </li>
-<li class="list-group-item" > imp_all.exp1()</li>
-<li class="list-group-item" > imp_all.exp2_Alias()</li>
-<li class="list-group-item" > imp_all.exp3()</li>
+<li class="list-group-item" > ${imp_all.myVar} </li>
+<li class="list-group-item" > ${imp_all.exp1()}</li>
+<li class="list-group-item" > ${imp_all.exp2alias()}</li>
+<li class="list-group-item" > ${imp_all.exp3()}</li>
 
 `;
