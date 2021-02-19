f4302a9873808413b1ae6b546cc6cd8d171828cd
xemi000 <se.zemichael.amsalu@gmail.com>
2021-02-20 00:46:24 +0300

    commited

--- Finished/asset/js/05(OOP2).js
+++ Finished/asset/js/05(OOP2).js
@@ -6,11 +6,13 @@ const PersonObj = {
     }
 }
 
-//2. Create Theon Object using Person Template
-var Theon = new PersonObj();
+//2. Create Theon Object using Person Template using Object.create
+    let Theon = Object.create(PersonObj)
 
 //3. Add firstName[Theon] and lastName[Greyjoy] 
+Theon.firstName ="Theon"
+Theon.lastName = "Greyjoy"
 
 
 //4. Display the greeting on Theon Object [remove the string when you have the object]
-Obj_C2.innerHTML = "Theon.greeting()";
+Obj_C2.innerHTML = Theon.greeting();
