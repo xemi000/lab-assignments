6623a168034a643fbf98e45ad6953ef6f13d9188
xemi000 <se.zemichael.amsalu@gmail.com>
2021-02-20 00:47:10 +0300

    commited

--- Finished/asset/js/05(OOP3).js
+++ Finished/asset/js/05(OOP3).js
@@ -1,29 +1,21 @@
 // 1. Create a Person_ES6 Class 
-function Person(firstName, lastName) {
-    this.firstName = firstName || "unknown";
-    this.lastName = lastName || "unknown"; 
-   } 
-   Person.prototype.getFullName = function () 
-   { 
-       return this.firstName + " " + this.lastName; 
-   } 
-   function Student(firstName, lastName, schoolName) 
-   { 
-        Person.call(this, firstName, lastName); 
-        this.schoolName = schoolName || "unknown"; 
-   }
-   
+class Person_ES6{
 
 
-//2. Create the constructor that initialize firstName and lastName
-
-
-//3. Define greeting() method that displays Full Name
-
-
-//4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
-
-var bran = Person("bran","stark")
-
-//5. Display the greeting on Bran Object [remove the string when you have the object]
-Obj_C3.innerHTML = "bran.getFullName()";
+    //2. Create the constructor that initialize firstName and lastName
+        constructor(firstName,lastName){
+            this.firstName = firstName
+            this.lastName=lastName
+        }
+    //3. Define greeting() method that displays Full Name
+    greeting(){
+        return this.firstName + this.lastName
+    }
+    
+    }
+    //4. Create  Object using ES6 class by passing firstName,lastName ["Bran Stark"]
+    const Bran = new Person_ES6("Bran","Stark")
+    
+    //5. Display the greeting on Bran Object [remove the string when you have the object]
+    Obj_C3.innerHTML = Bran.greeting();
+    
