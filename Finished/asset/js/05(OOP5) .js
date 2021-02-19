7880b27a14cfa1008dd54f8fdc0dc8109947e5c2
xemi000 <se.zemichael.amsalu@gmail.com>
2021-02-20 00:48:39 +0300

    commited

--- Finished/asset/js/05(OOP5) .js
+++ Finished/asset/js/05(OOP5) .js
@@ -10,17 +10,18 @@ class Person_ES6 {
 }
 
 //1. Make Customer class to extend from Person_ES6
-class Customer_ES6 {
+class Customer_ES6 extends Person_ES6{
 
     constructor(fName, lName) {
 
         //2. Call the super class [super(fName,lName)]
+        super(fName, lName)
 
     }
 }
 
 //3. Create Customer ["Jeor", "Mormont"]
-
+const cu = new Customer_ES6("jeor","Mormont")
 
 //4. Display the greeting on Jeor Customer Object [remove the string when you have the object]
-es6_proto.innerHTML = "cu.greeting()";
+es6_proto.innerHTML = cu.greeting();
